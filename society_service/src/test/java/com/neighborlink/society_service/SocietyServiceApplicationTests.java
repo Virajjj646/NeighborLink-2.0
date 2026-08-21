202ec1f75a1416ae5e1c8346141373afb783c249
Viraj <viraj.deglurkar@gmail.com>
2026-08-21 17:41:33 +0530

    Society initial

---
+++ society_service/src/test/java/com/neighborlink/society_service/SocietyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neighborlink.society_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
